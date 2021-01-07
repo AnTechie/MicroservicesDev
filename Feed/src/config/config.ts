f9116bb470184ea31abae9b9605adc44260c6416
Anjani Kodali <anjani.katabathuni@gmail.com>
2021-01-07 12:52:48 -0800

    feeds api changes. 1.Added deployment files,travis file,required packages to run the application

--- Feed/src/config/config.ts
+++ Feed/src/config/config.ts
@@ -1,14 +1,29 @@
+// export const config = {
+//   'username': process.env.POSTGRES_USERNAME,
+//   'password': process.env.POSTGRES_PASSWORD,
+//   'database': process.env.POSTGRES_DB,
+//   'host': process.env.POSTGRES_HOST,
+//   'dialect': 'postgres',
+//   'aws_region': process.env.AWS_REGION,
+//   'aws_profile': process.env.AWS_PROFILE,
+//   'aws_media_bucket': process.env.AWS_BUCKET,
+//   'url': process.env.URL,
+//   'jwt': {
+//     'secret': process.env.JWT_SECRET,
+//   },
+// };
+
 export const config = {
-  'username': process.env.POSTGRES_USERNAME,
-  'password': process.env.POSTGRES_PASSWORD,
-  'database': process.env.POSTGRES_DB,
-  'host': process.env.POSTGRES_HOST,
+  'username': "anjvendev1",
+  'password': "anjvendev1",
+  'database': "postgres",
+  'host': "anjvendev1.c7nucnuz9p7d.us-west-2.rds.amazonaws.com",
   'dialect': 'postgres',
-  'aws_region': process.env.AWS_REGION,
-  'aws_profile': process.env.AWS_PROFILE,
-  'aws_media_bucket': process.env.AWS_BUCKET,
-  'url': process.env.URL,
+  'aws_region': "us-west-2",
+  'aws_profile': "default",
+  'aws_media_bucket': "anj-ven-dev",
+  'url': "http://localhost:8080",
   'jwt': {
-    'secret': process.env.JWT_SECRET,
+    'secret': "hello",
   },
 };
